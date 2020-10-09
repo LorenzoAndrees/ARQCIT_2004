c8b4f6de55e57de3a98b5f82e15ecbed00e65dc7
LorenzoAndrees <lordandrees@gmail.com>
2020-10-09 18:28:52 -0300

    Ip añadida

--- conectar.py
+++ conectar.py
@@ -1,7 +1,7 @@
 from socket import create_connection
 
 # Conectar al servidor.
-with create_connection(("localhost", 6190)) as conn:
+with create_connection(("127.0.0.1", 5000)) as conn:
     print("Conectado al bus.")
     # Enviar datos.
     conn.sendall(b"Hola profe!")
